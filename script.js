31961eacf7c7e1e29fa04f21d7c8861d69b70f1c
Angelica Nascimento <107217901+angelicanascimentoo@users.noreply.github.com>
2024-06-17 08:54:38 -0300

    17.06.2024
    aula 8

--- script.js
+++ script.js
@@ -8,46 +8,98 @@ const perguntas = [
     {
         enunciado: "Você costuma reciclar seu lixo?",
         alternativas: [
-            "Sim",
-            "Não"
+            {
+                texto: "Sim",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Não",
+                afirmacao: "afirmacao"
+            }
+                 
         ]
     },
     {
         enunciado: "Quais são as principais ameaças á biodiversidade global?",
-        alternativas: [
-            "O desmatamento, exploração excessiva de recursos, presença de espécies invasoras e a contaminação ambiental",
-            "As principais ameaças á biodiversidade global são a melhoria das condições de vida humana e o aumento da área de reservas naturais"
+        alternativas: [ {
+            texto: "O desmatamento, exploração excessiva de recursos, presença de espécies invasoras e a contaminação ambiental",
+            afirmacao: "afirmacao"
+        },
+        {
+            texto: "As principais ameaças á biodiversidade global são a melhoria das condições de vida humana e o aumento da área de reservas naturais",
+            afirmacao: "afirmacao"
+        } 
+           
         ]
     },
     {
         enunciado: "Quais são os principais gases do efeito estufa e suas fontes?",
         alternativas: [
-            "O dióxido de carbono (CO2), o metano e o óxido nitroso",
-            "Oxigênio, o hidrogênio e o hélio"
+            {
+                texto: "O dióxido de carbono (CO2), o metano e o óxido nitroso",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Oxigênio, o hidrogênio e o hélio",
+                afirmacao: "afirmacao"
+            }
+            
         ]
     }, 
     {
         enunciado: "Quais são os principais impactos do plástico no meio ambiente?",
         alternativas: [
-            "Ajuda a fertilizar o solo, melhora a qualidade do ar e proporciona habitats seguros para várias espécies de animais aquáticos",
-            "Proliferam-se os lixões a céu aberto, contaminando a água dos rios e lençóis freáticos"
+            {
+                texto:  "Ajuda a fertilizar o solo, melhora a qualidade do ar e proporciona habitats seguros para várias espécies de animais aquáticos",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto:  "Proliferam-se os lixões a céu aberto, contaminando a água dos rios e lençóis freáticos",
+                afirmacao:"afirmacao"
+            } 
+           
         ]
     },
     {
         enunciado: "Como a poluição sonora afeta os seres humanos e a vida selvagem?",
         alternativas: [
-            "O ruído pode pertubar os padrões de reprodução, aleitamento e, inclusive, contribuir para a extinção de algumas espécies",
-            "A poluição sonora não afeta a vida selvagem nem dos seres humanos, muitos animais se adaptam e até prosperam em ambientes barulhentos, e os seres humanos não tem nenhum impacto negativo na saúde"
+            {
+                texto: "O ruído pode pertubar os padrões de reprodução, aleitamento e, inclusive, contribuir para a extinção de algumas espécies",
+                afirmacao:"afirmcao"
+            },
+            {
+            texto: "A poluição sonora não afeta a vida selvagem nem dos seres humanos, muitos animais se adaptam e até prosperam em ambientes barulhentos, e os seres humanos não tem nenhum impacto negativo na saúde",
+                afirmacao:"afirmacao"
+            }   
+            
         ]
     },
 ];
 
+
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 }
 
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSeleconada){
+    const afirmacoes = opcaoSeleconada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+}
 mostraPergunta();
