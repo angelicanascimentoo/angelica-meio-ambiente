7a03095430e0b896af46444a7055f4d8fa6462c5
Angelica Nascimento <107217901+angelicanascimentoo@users.noreply.github.com>
2024-07-01 08:51:13 -0300

    1-07
    projeto final

--- script.js
+++ script.js
@@ -10,26 +10,26 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Sim",
-                afirmacao: "afirmacao"
+                afirmacao: "muito bem! continue reciclando seu lixo para ajudar o meio ambiente."
             },
             {
                 texto: "Não",
-                afirmacao: "afirmacao"
+                afirmacao: "Você deveria começar a reciclar seu lixo, para ajudar o meio ambiente."
             }
-                 
+
         ]
     },
     {
         enunciado: "Quais são as principais ameaças á biodiversidade global?",
-        alternativas: [ {
+        alternativas: [{
             texto: "O desmatamento, exploração excessiva de recursos, presença de espécies invasoras e a contaminação ambiental",
-            afirmacao: "afirmacao"
+            afirmacao: "O desmatamento e a exploração indiscriminada de recursos naturais são fatores que agravam a contaminação ambiental e favorecem a disseminação de espécies invasoras."
         },
         {
             texto: "As principais ameaças á biodiversidade global são a melhoria das condições de vida humana e o aumento da área de reservas naturais",
-            afirmacao: "afirmacao"
-        } 
-           
+            afirmacao: "As principais ameaças á biodiversidade global resultam das atividades humanas e da destruição de habitats naturais, que comprometem a sobrevivência de inúmeras espécies. "
+        }
+
         ]
     },
     {
@@ -37,27 +37,27 @@ const perguntas = [
         alternativas: [
             {
                 texto: "O dióxido de carbono (CO2), o metano e o óxido nitroso",
-                afirmacao: "afirmacao"
+                afirmacao: "O dióxido de carbono (Co2), o metano (CH4) e o óxido nitroso (N20) são gases de efeito estufa que contribuem significativamente para o aquecimento global."
             },
             {
                 texto: "Oxigênio, o hidrogênio e o hélio",
-                afirmacao: "afirmacao"
+                afirmacao: "O oxigênio, o hidrogênio e o hélio são elementos químicos fundamentais, cada um com propriedades únicas e importantes para diversos processos naturais e industriais"
             }
-            
+
         ]
-    }, 
+    },
     {
         enunciado: "Quais são os principais impactos do plástico no meio ambiente?",
         alternativas: [
             {
-                texto:  "Ajuda a fertilizar o solo, melhora a qualidade do ar e proporciona habitats seguros para várias espécies de animais aquáticos",
-                afirmacao: "afirmacao"
+                texto: "Ajuda a fertilizar o solo, melhora a qualidade do ar e proporciona habitats seguros para várias espécies de animais aquáticos",
+                afirmacao: "A restauraação de ecossistemas aquáticos ajuda a fertilizar o solo, melhora a qualidade do ar e proporciona habitats seguros para várias espécies de animais."
             },
             {
-                texto:  "Proliferam-se os lixões a céu aberto, contaminando a água dos rios e lençóis freáticos",
-                afirmacao:"afirmacao"
-            } 
-           
+                texto: "Proliferam-se os lixões a céu aberto, contaminando a água dos rios e lençóis freáticos",
+                afirmacao: "Os lixões a céu aberto proliferam, contaminando a água dos rios e lençóis freáticos."
+            }
+
         ]
     },
     {
@@ -65,13 +65,13 @@ const perguntas = [
         alternativas: [
             {
                 texto: "O ruído pode pertubar os padrões de reprodução, aleitamento e, inclusive, contribuir para a extinção de algumas espécies",
-                afirmacao:"afirmcao"
+                afirmacao: "o ruído pode afetar negativamente os padrões de reprodução e aleitamento, contribuindo até mesmo para a extinção de certas espécies"
             },
             {
-            texto: "A poluição sonora não afeta a vida selvagem nem dos seres humanos, muitos animais se adaptam e até prosperam em ambientes barulhentos, e os seres humanos não tem nenhum impacto negativo na saúde",
-                afirmacao:"afirmacao"
-            }   
-            
+                texto: "A poluição sonora não afeta a vida selvagem nem dos seres humanos, muitos animais se adaptam e até prosperam em ambientes barulhentos, e os seres humanos não tem nenhum impacto negativo na saúde",
+                afirmacao: "A poluição sonora afeta muito os animais e os seres humanos também"
+            }
+
         ]
     },
 ];
@@ -82,7 +82,7 @@ let perguntaAtual;
 let historiaFinal = "";
 
 function mostraPergunta() {
-    if(atual >= perguntas.length){
+    if (atual >= perguntas.length) {
         mostraResultado();
         return;
     }
@@ -92,8 +92,8 @@ function mostraPergunta() {
     mostraAlternativas();
 }
 
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
@@ -101,17 +101,17 @@ function mostraAlternativas(){
     }
 }
 
-function respostaSelecionada(opcaoSeleconada){
-    const afirmacoes = opcaoSeleconada.afirmacoes;
+function respostaSelecionada(opcaoSeleconada) {
+    const afirmacoes = opcaoSeleconada.afirmacao;
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
 }
 
-function mostraResultado(){
+function mostraResultado() {
     caixaPerguntas.textContent = "Em 2024...";
     textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent= "";
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
